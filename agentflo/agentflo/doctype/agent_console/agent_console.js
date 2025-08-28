3f553fb3634f2c0530b47d79aeeeb417cedc8c35
Sanjusha_tridz <sanjusha@tridz.com>
2025-08-28 06:20:02 +0000

    fix:disable save action in agent console

--- agentflo/agentflo/doctype/agent_console/agent_console.js
+++ agentflo/agentflo/doctype/agent_console/agent_console.js
@@ -1,22 +1,31 @@
-// Copyright (c) 2025, Tridz Technologies Pvt Ltd and contributors
-
 frappe.ui.form.on("Agent Console", {
-	refresh(frm) {
-        frm.add_custom_button("Run Agent", function() {
-            console.log("Run Agent button clicked");
+    refresh(frm) {
+        frm.disable_save();
+
+        frm.set_value("agent_name", "");
+        frm.set_value("prompt", "");
+        frm.set_value("response", "");
+
+        frm.page.set_primary_action(__("Run Agent"), ($btn) => {
+            $btn.text(__("Running..."));
+
             frappe.call({
                 method: "agentflo.ai.agent_integration.run_agent_sync",
                 args: {
                     agent_name: frm.doc.agent_name,
-                    prompt: frm.doc.prompt
+                    prompt: frm.doc.prompt,
                 },
                 callback: function(r) {
-                    frm.doc.response = r.message.response;
-                    frm.refresh_field("response");
-                    console.log("Agent run completed", r.message.response);
+                    if (r.message) {
+                        frm.set_value("response", r.message.response || "");
+                    }
+                    $btn.text(__("Run Agent"));
+                },
+                error: function() {
+                    frappe.msgprint(__("Something went wrong while running the agent."));
+                    $btn.text(__("Run Agent"));
                 }
             });
-        },);
-    
-	},
+        });
+    },
 });
