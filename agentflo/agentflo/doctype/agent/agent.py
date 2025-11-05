89459a198d9325cebbe205a759a753451f893652
Sanjusha_tridz <sanjusha@tridz.com>
2025-11-05 11:40:49 +0000

    update:removed doc event dependencies from Agent

--- agentflo/agentflo/doctype/agent/agent.py
+++ agentflo/agentflo/doctype/agent/agent.py
@@ -5,19 +5,11 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from agentflo.ai.agent_hooks import clear_doc_event_agents_cache
-from frappe.utils.safe_exec import get_safe_globals, safe_eval
-
-
-def get_context(doc):
-	return {"doc": frappe._dict(doc), **get_safe_globals()}
 
 class Agent(Document):
 	def validate(self):
 		if not self.instructions:
 			frappe.throw(_("Please provide an instruction for this AI Agent."))
-		if self.is_scheduled and self.is_doc_event:
-			frappe.throw(_("An Agent cannot be both Scheduled and Doc Event based. Please choose only one."))
-		self.validate_condition()
 		if self.allow_chat == 1 and self.persist_conversation == 0:
 			frappe.throw(_("An agent cannot be allowed in Agent Chat when persistent conversation is off."))
 
@@ -34,14 +26,4 @@ class Agent(Document):
 
 	def on_trash(self):
 		clear_doc_event_agents_cache()
-	
-	def validate_condition(self):
-		if not self.condition:
-			return
-
-		temp_doc = frappe.new_doc(self.reference_doctype)
-		try:
-			frappe.safe_eval(self.condition, None, get_context(temp_doc.as_dict()))
-		except Exception:
-			frappe.throw(_("The Condition '{0}' is invalid").format(self.condition))
 		
