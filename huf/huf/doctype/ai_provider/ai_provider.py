82d3f4eaa6068e305163ddd3534cc09f65aaa8b6
Sanjusha_tridz <sanjusha@tridz.com>
2025-12-20 06:15:27 +0000

    feat : finding provider settings using button

--- huf/huf/doctype/ai_provider/ai_provider.py
+++ huf/huf/doctype/ai_provider/ai_provider.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2025, Tridz Technologies Pvt Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AIProvider(Document):
 	pass
+
+@frappe.whitelist()
+def get_provider_settings(provider_name):
+    """
+    Finds Single DocTypes that match the pattern '{Provider} % Settings'
+    """
+    if not provider_name:
+        return []
+    
+    candidates = frappe.db.sql("""
+        SELECT name FROM `tabDocType`
+        WHERE issingle = 1 
+        AND name LIKE %s
+        AND name LIKE '%%Settings'
+    """, (f"%{provider_name}%",), as_dict=True)
+    
+    return [c.name for c in candidates]
