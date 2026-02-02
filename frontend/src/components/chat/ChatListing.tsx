05f8aab606863f34f8333345602b93641fe0e7a4
shahzadbinshahajhan <shahzadbinshahjahan@gmail.com>
2026-02-02 09:18:25 +0000

    update: init chat listing in recents

--- frontend/src/components/chat/ChatListing.tsx
+++ frontend/src/components/chat/ChatListing.tsx
@@ -8,7 +8,7 @@ export default function ChatListing(){
     return (
         <div className="h-full min-h-screen min-w-96 bg-sidebar p-4 space-y-4">
             <ChatListHeader/>
-            <Tabs defaultValue="agent">
+            <Tabs defaultValue="agent" className="space-y-4">
                 <TabsList className="w-full">
                     {LIST_TABS.map((tab)=>(
                         <TabsTrigger className="w-1/2 space-x-2 text-xs font-medium" value={tab.value}>
@@ -42,7 +42,7 @@ function ChatListingAgent(){
                     <AccordionTrigger className="group gap-2 mb-1 py-1 px-1 hover:bg-zinc-200 cursor-pointer select-none rounded-lg" arrowPosition="left">
                         <div className="flex-1 flex gap-x-2 items-center">
                             <ChatAvatar variant="listing_ai">DE</ChatAvatar>
-                            <span className="text-sm font-medium truncate text-zinc-500 group-hover:text-zinc-900 transition-colors">DevOps Copilot</span>
+                            <span className="text-sm font-medium truncate text-zinc-500 group-hover:text-zinc-900 transition-colors">Agent Name Here</span>
                         </div>
                         <span className="text-[10px] text-zinc-400 bg-zinc-200 px-1.5 py-0.5 rounded-full border border-zinc-200 ml-auto">2</span>
                     </AccordionTrigger>
@@ -63,7 +63,31 @@ function ChatListingAgent(){
 function ChatListingRecent(){
     return (
         <TabsContent value="recents">
-            hihiii
+            <span className="px-2 text-xs font-medium text-zinc-400 uppercase tracking-wider">THIS WEEK</span>
+            {/* Chat Listing Here */}
+            <div className="mt-2 space-y-1">
+                {/* Active conversation */}
+                <div className="flex p-3 gap-2 items-center rounded-lg cursor-pointer transition-all border bg-zinc-200 border-zinc-200">
+                    <div className="flex flex-1 gap-2 items-center">
+                        <ChatAvatar variant="chat_ai">DE</ChatAvatar>
+                        <div className="mb-1">
+                            <span className="text-sm font-medium truncate text-zinc-900">Conversation Header here</span>
+                            <p className="text-xs truncate text-zinc-500">Agent Name heere</p>
+                        </div>
+                    </div>
+                    <span className="mt-1.5 text-[10px] text-zinc-400 flex-shrink-0 justify-self-end self-start">2m ago</span>
+                </div>
+                <div className="flex p-3 gap-2 items-center rounded-lg cursor-pointer transition-all border border-transparent bg-transparent hover:bg-zinc-200">
+                    <div className="flex flex-1 gap-2 items-center">
+                        <ChatAvatar variant="chat_ai">DE</ChatAvatar>
+                        <div className="mb-1">
+                            <span className="text-sm font-medium truncate text-zinc-900">Conversation Header here</span>
+                            <p className="text-xs truncate text-zinc-500">Agent Name heere</p>
+                        </div>
+                    </div>
+                    <span className="mt-1.5 text-[10px] text-zinc-400 flex-shrink-0 justify-self-end self-start">2m ago</span>
+                </div>
+            </div>
         </TabsContent>
     )
 }
