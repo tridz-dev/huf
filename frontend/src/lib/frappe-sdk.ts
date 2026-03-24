c97e00e80517ab052800b0112c811895fa4d5344
shzdb <shahzad@tridz.com>
2026-03-25 00:48:47 +0400

    feat: knowledge api integration

--- frontend/src/lib/frappe-sdk.ts
+++ frontend/src/lib/frappe-sdk.ts
@@ -8,3 +8,4 @@ export const frappe = new FrappeApp(frappeUrl);
 export const auth = frappe.auth();
 export const db = frappe.db();
 export const call = frappe.call();
+export const file = frappe.file();
