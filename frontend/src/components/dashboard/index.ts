edc46b02fcee91786636ce9d53ac21d194b33fdb
shzdb <shahzad@tridz.com>
2026-07-07 15:40:40 +0530

    feat(frontend): add route-level page loader fallbacks
    Add DashboardPageLoader, GridPageLoader, and TablePageLoader to replace
    generic spinners during lazy route loading with layout-matched skeletons.

--- frontend/src/components/dashboard/index.ts
+++ frontend/src/components/dashboard/index.ts
@@ -3,6 +3,9 @@ export { PageSection } from './layouts/PageSection';
 
 export { GridView } from './views/GridView';
 export { SkeletonGridView } from './views/SkeletonGridView';
+export { SkeletonListRow } from './views/SkeletonListRow';
+export { SkeletonListView } from './views/SkeletonListView';
+export { SkeletonTable } from './views/SkeletonTable';
 export { ActiveAgentsTab } from './views/ActiveAgentsTab';
 export { ActiveFlowsTab } from './views/ActiveFlowsTab';
 export { RecentExecutionsTab } from './views/RecentExecutionsTab';
@@ -14,3 +17,9 @@ export { ItemCard } from './cards/ItemCard';
 export { SkeletonCard } from './cards/SkeletonCard';
 
 export { LoadMoreButton } from './LoadMoreButton';
+export { PageListFooter } from './PageListFooter';
+
+export { FilterBarSkeleton } from './loaders/FilterBarSkeleton';
+export { DashboardPageLoader } from './loaders/DashboardPageLoader';
+export { GridPageLoader } from './loaders/GridPageLoader';
+export { TablePageLoader } from './loaders/TablePageLoader';
